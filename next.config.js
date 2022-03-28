a9dd31396af739ff2af53f4076e1c6ad85110d22
eb-73 <81568567+eb-73@users.noreply.github.com>
2022-03-28 19:12:00 +0430

    delete cofig file

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  env: {
-    GOOGLE_CLIENT_ID:
-      "468368942474-jgmn0mhi8dg78067mhhvd0u8g6m5a9oc.apps.googleusercontent.com",
-    GOOGLE_CLIENT_SECRET: "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",
-    SECRET: "LlKq6ZtYbr+hTC073$HkBAh9/h2HwMfsFo4hrfCx5mLg=",
-  },
-};
